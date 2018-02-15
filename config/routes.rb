3404584bec7b6d82c0a745573cee44392e602e36
Joel Savitz <theyoyojo@gmail.com>
2018-02-14 21:23:19 -0500

    Add banger

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :aframes
   post 'aframes/random' => 'aframes#random'
   root 'aframes#new'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
