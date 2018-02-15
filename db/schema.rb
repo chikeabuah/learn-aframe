0a94a2628c11f18c3492c04128da99b6604237a3
Chris Berns <cberns223@gmail.com>
2018-02-15 13:19:36 -0500

    Convert to PG for Heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180209222413) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "aframes", force: :cascade do |t|
     t.text "jscode"
     t.text "htmlcode"
